82515271ce7180f751f8d6a61c2b228174712112
fajaredip <fajar.e@elabram.com>
2024-08-14 10:11:03 +0700

    test push

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,22 @@
 <template>
 	<div class="flex flex-col">
-		<section><div class="w-full min-h-[20rem] bg-amber-500"></div></section>
-		<section><div class="w-full min-h-[20rem] bg-[#f6f6f6]"></div></section>
-		<section
-			class="flex w-full p-5 bg-[linear-gradient(45deg,#282560,#3486c9)] text-white"
-		>
+		<section>
+			<div class="flex items-center justify-center w-full min-h-[20rem] bg-amber-500">
+				<h1>Hero Section</h1>
+			</div>
+		</section>
+		<section>
+			<div class="flex items-center justify-center w-full min-h-[20rem] bg-[#f6f6f6]">
+				Featured Section
+			</div>
+		</section>
+		<section class="flex w-full p-5 bg-[linear-gradient(45deg,#282560,#3486c9)] text-white">
 			<div class="flex flex-col w-[50rem]">
 				<h1 class="text-2xl font-semibold mb-3">About Elabram</h1>
 				<p class="mb-3">
-					Elabram HR Solution focuses on human resources solutions. The company
-					covers every HR needs: from manpower/temporary staff and payroll
-					services by HR Outsourcing solution, permanent staff, expatriate
-					providers, Executive Search by Recruitment Solutions.
+					Elabram HR Solution focuses on human resources solutions. The company covers every HR
+					needs: from manpower/temporary staff and payroll services by HR Outsourcing solution,
+					permanent staff, expatriate providers, Executive Search by Recruitment Solutions.
 				</p>
 				<h1 class="text-2xl font-semibold mb-3">Our Services</h1>
 				<div class="flex mb-3">
@@ -43,29 +48,19 @@
 				<div>
 					<ul class="flex items-center justify-end gap-3">
 						<li>
-							<a href="#"
-								><img src="/icon-facebook.svg" alt="" class="w-10"
-							/></a>
+							<a href="#"><img src="/icon-facebook.svg" alt="" class="w-10" /></a>
 						</li>
 						<li>
-							<a href="#"
-								><img src="/icon-twitter.svg" alt="" class="w-10"
-							/></a>
+							<a href="#"><img src="/icon-twitter.svg" alt="" class="w-10" /></a>
 						</li>
 						<li>
-							<a href="#"
-								><img src="/icon-linked-in.svg" alt="" class="w-10"
-							/></a>
+							<a href="#"><img src="/icon-linked-in.svg" alt="" class="w-10" /></a>
 						</li>
 						<li>
-							<a href="#"
-								><img src="/icon-instagram.svg" alt="" class="w-10"
-							/></a>
+							<a href="#"><img src="/icon-instagram.svg" alt="" class="w-10" /></a>
 						</li>
 						<li>
-							<a href="#"
-								><img src="/icon-youtube.svg" alt="" class="w-10"
-							/></a>
+							<a href="#"><img src="/icon-youtube.svg" alt="" class="w-10" /></a>
 						</li>
 					</ul>
 				</div>
